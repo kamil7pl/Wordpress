65ada5cdeb198f361796a2facd3c1eab88d6d5f7
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-05-27 14:12:58 +0200

    Add files via upload

---
+++ wtyczkaKamil/wtyczkaKamil.php
@@ -0,0 +1,18 @@
+<?php
+/*
+*Plugin Name: Moja wtyczka
+*Description: opis
+*Version: 1.0
+*Author: ja
+*Author URI: 
+*Plugin URI:
+*License:
+*/
+function moja_wtyczka(){
+	add_menu_page('Strona główna wtyczki', 'Moja wtyczka Kamil', 'administrator', 'xx-glowna', 'yy_glowna', '', 24);
+}
+add_action('admin_menu', 'moja_wtyczka', '');
+function yy_glowna(){
+	echo 2+15;
+}
+?>
